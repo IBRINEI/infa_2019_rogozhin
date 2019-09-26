0c79186beef66371f4f7689067ea88252108587d
IBRINEI <55546925+IBRINEI@users.noreply.github.com>
2019-09-26 19:06:12 +0300

    Update 4HW2.py
    i am doing something

--- 4HW2.py
+++ 4HW2.py
@@ -3,31 +3,55 @@ from graph import *
 windowSize(1760, 769)
 canvasSize(1760, 769)
 
+def Hair(color, shift_from_left_guy):
+    brushColor(color)
+    a = shift_from_left_guy
+    polygon([(293 + a, 275), (336 + a, 219), (267 + a, 205)])
+    polygon([(316 + a, 230), (378 + a, 200), (329 + a, 166)])
+    polygon([(362 + a, 201), (431 + a, 186), (377 + a, 149)])
+    polygon([(417 + a, 184), (476 + a, 180), (436 + a, 144)])
+    polygon([(457 + a, 187), (509 + a, 186), (486 + a, 143)])
+    polygon([(498 + a, 178), (543 + a, 200), (525 + a, 151)])
+    polygon([(532 + a, 187), (586 + a, 220), (573 + a, 152)])
+    polygon([(576 + a, 206), (621 + a, 253), (615 + a, 182)])
+    polygon([(608 + a, 235), (644 + a, 289), (674 + a, 218)])
 
+def Main_Body(color, shift_from_left_guy):
+    penColor(color)
+    brushColor(color)
+    circle(463 + shift_from_left_guy, 769, 300)
 
-penColor('orange')
-brushColor('orange')
-circle(463, 769, 300)
+def Head(shift_from_left_guy):
+    brushColor(233, 200, 176)
+    penColor(200, 200, 200)
+    circle(463, 384, 200)
+    penColor(0, 0, 0)
+    
+def Eyes(color_red, color_green, color_blue, shift_from_left_guy):
+    brushColor(color_red, color_green, color_blue)
+    penColor('black')
+    circle(528 + shift_from_left_guy, 349, 35)
+    circle(398 + shift_from_left_guy, 349, 35)
+    brushColor('black')
+    circle(528 + shift_from_left_guy, 349, 10)
+    circle(398 + shift_from_left_guy, 349, 10)
+    
+def Nose_And_Mouth(shift_from_left_guy):
+    a = shift_from_left_guy
+    brushColor('brown')
+    polygon([(483 + a, 394), (443 + a, 394), (463 + a, 424)])
+    #Nose is above; mouth is below
+    brushColor('red')
+    polygon([(333 + a, 454), (613 + a, 454), (463 + a, 534)])
+    
+#left guy start
+Main_body('orange', 0)
 
-brushColor(233, 200, 176)
-penColor(200, 200, 200)
-circle(463, 384, 200)
-penColor(0, 0, 0)
+Head(0)
 
-brushColor(100, 100, 255)
-penColor('black')
-circle(528, 349, 35)
-circle(398 , 349, 35)
-brushColor('black')
-circle(528, 349, 10)
-circle(398 , 349, 10)
+Eyers(100, 100, 255, 0)
 
-brushColor('brown')
-polygon([(483, 394), (443, 394), (463, 424)])
-
-brushColor('red')
-polygon([(333, 454), (613, 454), (463, 534)])
-
+Nose_And_Mouth(0)
 
 
 brushColor(233, 200, 176)
@@ -45,43 +69,17 @@ brushColor('orange')
 polygon([(300, 629), (220, 629), (210, 539), (280, 489), (320, 549)])
 polygon([(700, 629), (620, 629), (610, 539), (680, 489), (720, 549)])
 
-brushColor('purple')
-polygon([(293,275), (336,219), (267,205)])
-polygon([(316,230), (378,200), (329,166)])
-polygon([(362,201), (431,186), (377,149)])
-polygon([(417,184), (476,180), (436,144)])
-polygon([(457,187), (509,186), (486,143)])
-polygon([(498,178), (543,200), (525,151)])
-polygon([(532,187), (586,220), (573,152)])
-polygon([(576,206), (621,253), (615,182)])
-polygon([(608,235), (644,289), (674,218)])
+Hair('purple', 0)
+#left guy end
 
+#second guy start
+Main_Body('green', 800)
 
-#copy start
-penColor('orange')
-brushColor('green')
-circle(1263, 769, 300)
+Head(800)
 
-brushColor(233, 200, 176)
-penColor(200, 200, 200)
-circle(1263, 384, 200)
-penColor(0, 0, 0)
+Eyes(191, 200, 183, 800)
 
-brushColor(191, 200, 183)
-penColor('black')
-circle(1328, 349, 35)
-circle(1198 , 349, 35)
-brushColor('black')
-circle(1328, 349, 10)
-circle(1198 , 349, 10)
-
-brushColor('brown')
-polygon([(1283, 394), (1243, 394), (1263, 424)])
-
-brushColor('red')
-polygon([(1133, 454), (1413, 454), (1263, 534)])
-
-
+Nose_And_Mouth(800)
 
 brushColor(233, 200, 176)
 penColor(200, 200, 200)
@@ -98,21 +96,11 @@ brushColor('green')
 polygon([(1100, 629), (1020, 629), (1010, 539), (1080, 489), (1120, 549)])
 polygon([(1500, 629), (1420, 629), (1410, 539), (1480, 489), (1520, 549)])
 
-brushColor('yellow')
-polygon([(1093,275), (1136,219), (1067,205)])
-polygon([(1116,230), (1178,200), (1129,166)])
-polygon([(1162,201), (1231,186), (1177,149)])
-polygon([(1217,184), (1276,180), (1236,144)])
-polygon([(1257,187), (1309,186), (1286,143)])
-polygon([(1298,178), (1343,200), (1325,151)])
-polygon([(1332,187), (1386,220), (1373,152)])
-polygon([(1376,206), (1421,253), (1415,182)])
-polygon([(1408,235), (1444,289), (1474,218)])
-#copy end
+Hair('yellow', 800)
+#second guy end
 brushColor('black')
 
-
+#label on top; may not work with old OS/PC
 label('PYTHON is REALLY AMAZING!', 00, 00, font=('Arial 32', 90, 'bold'), bg='green', foreground='black')
 
 run()
-
