d4b770feb5a6cf354bcede34270cefc9586c6c76
IBRINEI <55546925+IBRINEI@users.noreply.github.com>
2019-10-05 12:30:02 -0700

    Update task.py

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
-from math import sin, cos
+from math import sin, cos, pi
+
 
 def prime(a):
     m = []
@@ -9,7 +10,7 @@ def prime(a):
         for j in range(n):
             if (i + 3) % m[j] == 0:
                 q = False
-        if q == True:
+        if q:
             m.append(i + 3)
             n = n + 1
         else:
@@ -23,7 +24,7 @@ def sort(m):
         for j in range(len(m) - i):
             if m[j + i] < m[q]:
                 q = j + i
-        m[i],m[q] = m[q], m[i]
+        m[i], m[q] = m[q], m[i]
         q = i + 1
     print(m)
 
@@ -32,13 +33,13 @@ def rotate_square(square, angle):
     mid_x = 0
     mid_y = 0
     square1 = [[0, 0], [0, 0], [0, 0], [0, 0]]
-    for i in range(3):
+    for i in range(4):
         mid_x = mid_x + square[i][0]
         mid_y = mid_y + square[i][1]
-    mid = (mid_x/4, mid_y/4)
-    for i in range(3):
-        square1[i][0] = (square[i][0] - mid_x/4)*cos(angle) - (square[i][1] - mid_y/4)*sin(angle)
-        square1[i][1] = (square[i][0] - mid_x/4)*sin(angle) + (square[i][1] - mid_y/4)*cos(angle)
+    for i in range(4):
+        square1[i][0] = (square[i][0] - mid_x/4)*cos(angle) - (square[i][1] - mid_y/4)*sin(angle) + mid_x/4
+        square1[i][1] = (square[i][0] - mid_x/4)*sin(angle) + (square[i][1] - mid_y/4)*cos(angle) + mid_y/4
+        square1[i] = tuple(square1[i])
     print(square1)
 
 
@@ -49,8 +50,28 @@ def market(lst, money):
             m.append(key)
     print(m)
 
+def no_duble(m):
+    set1 = set(m)
+    print(list(set1))
+
+def difference(m1, m2):
+    q = True
+    m3 = []
+    for i in m1:
+        for j in m2:
+            if i == j:
+                q = False
+        if q:
+            m3.append(i)
+        else:
+            q = True
+    print(list(set(m3)))
 
 prime(1000)
 sort([6, 4, 2, 5, 35, 3, 34, 25, 0])
-rotate_square(((1, 1), (1, -1), (-1, -1), (-1, 1)), 0.785)
+rotate_square(((1, 1), (1, -1), (-1, -1), (-1, 1)), pi)
 market({'banana': 11, 'apple': 5, 'steak': 100}, 50)
+no_duble([1, 1, 1, 1, 2, 2, 2, 3, 4, 5, 5])
+difference([1, 2, 3, 2], [3, 4])
+
+
